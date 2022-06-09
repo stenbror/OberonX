fc4fea38654526044e4898bfec85efc7dce780fa
stenbror <stenbror@hotmail.com>
2022-06-09 18:59:54 +0000

    Build script updated for building g++ and clang++ version of OberonX

--- build.sh
+++ build.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+echo "Building the Gnu G++ version"
  g++ -o obx main.cc Tokenizer.cc Parser.cc ASTNode.cc
  strip obx
  
+ echo "Building the clang++ version"
+ clang++ -o obx_clang main.cc Tokenizer.cc Parser.cc ASTNode.cc
+ strip obx_clang
+
+ ls -la obx*
